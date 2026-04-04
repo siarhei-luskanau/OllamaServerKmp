a6a26ebc113c9d6bc145015d3aa2c828882a71a0
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2026-04-04 06:15:53 +0200

    Initialize Kotlin Multiplatform project with Compose Multiplatform

---
+++ diApp/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("composeMultiplatformConvention")
+    alias(libs.plugins.roborazzi)
+}
+
+kotlin {
+    android.namespace = "template.di.app"
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.androidx.datastore.core.okio)
+            implementation(projects.core.coreCommon)
+            implementation(projects.core.corePref)
+            implementation(projects.navigation)
+            implementation(projects.ui.uiCommon)
+            implementation(projects.ui.uiMain)
+            implementation(projects.ui.uiSplash)
+        }
+
+        androidHostTest.dependencies {
+            implementation(libs.robolectric)
+            implementation(libs.roborazzi)
+            implementation(libs.roborazzi.compose)
+        }
+
+        iosTest.dependencies {
+            implementation(libs.roborazzi.compose.ios)
+        }
+    }
+}
+
+// Directory for reference images
+roborazzi.outputDir.set(file("src/screenshots"))
---
+++ ui/uiMain/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("composeMultiplatformConvention")
+    alias(libs.plugins.roborazzi)
+}
+
+kotlin {
+    android.namespace = "template.ui.main"
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core.coreCommon)
+            implementation(projects.core.corePref)
+            implementation(projects.ui.uiCommon)
+        }
+
+        androidHostTest.dependencies {
+            implementation(libs.robolectric)
+            implementation(libs.roborazzi)
+            implementation(libs.roborazzi.compose)
+        }
+
+        iosTest.dependencies {
+            implementation(libs.roborazzi.compose.ios)
+        }
+    }
+}
+
+// Directory for reference images
+roborazzi.outputDir.set(file("src/screenshots"))
